7598f4169976facf3c56b98c33f22dd24317f8c6
Sofia Shkidchenko <sofiashkidchenko@Sofias-MacBook-Pro-7.local>
2024-11-19 19:31:11 -0500

    done

---
+++ task1/using-js-to-make-changes.js
@@ -0,0 +1,8 @@
+
+
+
+document.title = "Task 1 by Sofia Shkidchenko";
+
+document.querySelector("header h1").textContent = "Task 1";
+document.querySelector("header p").textContent = "I'm Sofia Shkidchenko!";
+document.querySelector("section h2").textContent = "List of things I like";
